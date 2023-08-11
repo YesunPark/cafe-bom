b78ebcf16bf8b55dabd55a97de9fb0cb8d9584be
YesunPark <lynn08082@gmail.com>
2023-08-12 00:58:24 +0900

    init

---
+++ src/main/java/com/zerobase/CafeBom/CafeBomApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.CafeBom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/CafeBom/CafeBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.CafeBom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
