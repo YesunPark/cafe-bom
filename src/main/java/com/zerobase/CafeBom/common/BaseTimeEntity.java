43798569250a1294808d9ab912c8d23a6d67a6e7
YesunPark <lynn08082@gmail.com>
2023-08-14 22:11:03 +0900

    Refactor: BaseTimeEntity abstract class 로 변경

--- src/main/java/com/zerobase/CafeBom/common/BaseTimeEntity.java
+++ src/main/java/com/zerobase/CafeBom/common/BaseTimeEntity.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime createdAt;
