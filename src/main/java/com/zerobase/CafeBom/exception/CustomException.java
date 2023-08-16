034770bd6c424098158a3d42693fc399af002685
YesunPark <lynn08082@gmail.com>
2023-08-16 09:15:18 +0900

    Feat: ExceptionController 생성, 공통 에러 응답 형식 설정

--- src/main/java/com/zerobase/CafeBom/exception/CustomException.java
+++ src/main/java/com/zerobase/CafeBom/exception/CustomException.java
@@ -3,16 +3,19 @@ package com.zerobase.CafeBom.exception;
 import com.zerobase.CafeBom.type.ErrorCode;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException {
 
     private final ErrorCode errorCode;
     private final String errorMessage;
+    private final HttpStatus errorStatus;
 
     public CustomException(ErrorCode errorCode) {
-        super(errorCode.getDescription()); // 이 부분 ExceptionHandler 로 처리하라고 피드백 받음.
+        super(errorCode.getDescription());
         this.errorCode = errorCode;
         this.errorMessage = errorCode.getDescription();
+        this.errorStatus = errorCode.getHttpStatus();
     }
 }
