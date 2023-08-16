55dc8ecb1cfff64d89103eb95a0ea04a6d1d241c
KWY <sunlake123@naver.com>
2023-08-16 19:14:25 +0900

    Remove: 테스트 용 Menu, MenuRepository, MenuRepositoryTest 파일 삭제

--- src/main/java/com/zerobase/CafeBom/option/domain/entity/MenuOptionCategory.java
+++ src/main/java/com/zerobase/CafeBom/option/domain/entity/MenuOptionCategory.java
@@ -1,11 +1,20 @@
 package com.zerobase.CafeBom.option.domain.entity;
 
 import com.zerobase.CafeBom.common.BaseTimeEntity;
-import com.zerobase.CafeBom.menu.domain.entity.Menu;
-import lombok.*;
-
-import javax.persistence.*;
+import java.awt.Menu;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
