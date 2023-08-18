1b51fb037b9c2a7e5de25de6262f600453c3c7c2
bingsoo95 <kimminsu403@naver.com>
2023-08-18 21:51:32 +0900

    Refactor: OrderCookingTime 코드 수정

--- src/main/java/com/zerobase/CafeBom/type/OrderCookingTime.java
+++ src/main/java/com/zerobase/CafeBom/type/OrderCookingTime.java
@@ -4,8 +4,8 @@ public enum OrderCookingTime {
 
   _5_TO_10_MINUTES("5분 ~ 10분"),
   _10_TO_15_MINUTES("10분 ~ 15분"),
-  _15_TO_20_MINUTES("5분 ~ 10분"),
-  _20_TO_25_MINUTES("10분 ~ 15분"),
+  _15_TO_20_MINUTES("15분 ~ 20분"),
+  _20_TO_25_MINUTES("20분 ~ 25분"),
   OVER_25_MINUTES("25분 이상");
 
   private final String displayName;
