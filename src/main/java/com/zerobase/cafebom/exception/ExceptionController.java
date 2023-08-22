f086d8ecdb5180fe5b03325f372f6fe1d42ed0dd
YesunPark <lynn08082@gmail.com>
2023-08-23 00:07:18 +0900

    Merge: main 병합

--- src/main/java/com/zerobase/cafebom/exception/ExceptionController.java
+++ src/main/java/com/zerobase/cafebom/exception/ExceptionController.java
@@ -3,17 +3,35 @@ package com.zerobase.cafebom.exception;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class ExceptionController {
 
-    @ExceptionHandler({CustomException.class})
+    // 컨트롤러 DTO validation 핸들러-yesun-23.08.21
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<MethodInvalidResponse> methodInvalidException(
+        final MethodArgumentNotValidException e
+    ) {
+        BindingResult bindingResult = e.getBindingResult();
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(MethodInvalidResponse.builder()
+                .errorCode(bindingResult.getFieldErrors().get(0).getCode())
+                .errorMessage(bindingResult.getFieldErrors().get(0).getDefaultMessage())
+                .build());
+    }
+
+    // CustiomException 핸들러-yesun-23.08.21
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<ExceptionResponse> customRequestException(final CustomException c) {
-        log.error("Api Exception => {}", c.getErrorCode());
+        log.error("Api Exception => {}, {}", c.getErrorCode(), c.getErrorMessage());
         return ResponseEntity
             .status(c.getErrorStatus())
             .body(ExceptionResponse.builder()
@@ -21,6 +39,14 @@ public class ExceptionController {
                 .errorMessage(c.getErrorMessage())
                 .build()
             );
+    }
+
+    @Getter
+    @Builder
+    public static class MethodInvalidResponse {
+
+        private String errorCode;
+        private String errorMessage;
     }
 
     @Getter
