aee4d2482c5a4b15cb0842115c5088a0481827ab
bingsoo95 <93966368+bingsoo95@users.noreply.github.com>
2023-08-22 23:32:29 +0900

    Refactor: merge 실수로 인한 Order 테이블명, 클래스명 재변경(Order -> Orders) (#18)
    * Refactor: merge 실수로 인한 Order 테이블명, 클래스명 재변경(Order -> Orders)
    
    ---------

--- src/main/java/com/zerobase/cafebom/orders/repository/OrdersRepository.java
+++ src/main/java/com/zerobase/cafebom/orders/repository/OrdersRepository.java
@@ -1,10 +1,10 @@
 package com.zerobase.cafebom.orders.repository;
 
-import com.zerobase.cafebom.orders.domain.entity.Order;
+import com.zerobase.cafebom.orders.domain.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
+public interface OrdersRepository extends JpaRepository<Orders,Long> {
 
 }
