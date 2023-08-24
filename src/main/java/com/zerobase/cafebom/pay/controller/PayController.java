6f0de943fff498f43f4b29e33c596a5cf93ba867
YesunPark <lynn08082@gmail.com>
2023-08-24 15:09:48 +0900

    Refactor: PayController api url 명세서에 맞게 변경 및 테스트컨트롤러 삭제

--- src/main/java/com/zerobase/cafebom/pay/controller/PayController.java
+++ src/main/java/com/zerobase/cafebom/pay/controller/PayController.java
@@ -9,18 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/auth/pay")
 public class PayController {
 
     private final PayService payService;
 
-    @RequestMapping("/pay")
+    @GetMapping
     public String payKakaoQR(Model model) {
         payService.payKakaoQR(model);
         return "pay";
-    }
-
-    @GetMapping("/test")
-    public String pay(){
-        return "";
     }
 }
