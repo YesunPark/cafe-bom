d86c4dad8b1d117a788f592b2e7501374c60c980
YesunPark <lynn08082@gmail.com>
2023-08-24 13:29:51 +0900

    Feat: OrdersProduct, OrdersProductOption 엔티티, 레포지토리 구현

---
+++ src/main/java/com/zerobase/cafebom/ordersproduct/domain/entity/OrdersProduct.java
@@ -0,0 +1,37 @@
+package com.zerobase.cafebom.ordersproduct.domain.entity;
+
+import com.zerobase.cafebom.common.BaseTimeEntity;
+import com.zerobase.cafebom.orders.domain.entity.Orders;
+import com.zerobase.cafebom.product.domain.entity.Product;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrdersProduct extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orders_id")
+    private Orders orders;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
