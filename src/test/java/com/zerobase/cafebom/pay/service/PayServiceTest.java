9ba5f63350b9bb9cabf187a64e17b39010e9dc4f
YesunPark <lynn08082@gmail.com>
2023-08-26 04:27:30 +0900

    Test: 주문 저장 서비스 테스트파일 생성

---
+++ src/test/java/com/zerobase/cafebom/pay/service/PayServiceTest.java
@@ -0,0 +1,47 @@
+package com.zerobase.cafebom.pay.service;
+
+import static org.mockito.BDDMockito.given;
+
+import com.zerobase.cafebom.member.repository.MemberRepository;
+import com.zerobase.cafebom.member.security.TokenProvider;
+import com.zerobase.cafebom.pay.service.dto.OrdersAddDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@ExtendWith(MockitoExtension.class)
+class PayServiceTest {
+
+    @Spy
+    @InjectMocks
+    private PayService payService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private TokenProvider tokenProvider;
+
+    String token = "Bearer token";
+
+
+    @Test
+    @DisplayName("주문 저장 성공")
+    void successAddOrders() {
+        // given
+        given(tokenProvider.getId(token)).willReturn(2L);
+//        given(memberRepository.findById(request.getUserId()))
+//            .willReturn(Optional.ofNullable(userEntity));
+//        given(accountRepository.save(any())).willReturn(accountEntity);
+
+        // when
+
+        // then
+    }
+}
