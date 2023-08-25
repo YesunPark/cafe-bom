1498fac1825d85d8bf5a77615318e128014d27c7
KWY <sunlake123@naver.com>
2023-08-26 07:40:29 +0900

    Fix: security 패키지 이동에 따른 import문 수정

--- src/test/java/com/zerobase/cafebom/product/controller/ProductControllerTest.java
+++ src/test/java/com/zerobase/cafebom/product/controller/ProductControllerTest.java
@@ -8,10 +8,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.zerobase.cafebom.member.security.TokenProvider;
 import com.zerobase.cafebom.member.service.AuthService;
 import com.zerobase.cafebom.product.service.ProductService;
 import com.zerobase.cafebom.product.service.dto.ProductDto;
+import com.zerobase.cafebom.security.TokenProvider;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
