3307f06fdbc6bffc6a815abb5ca5a6459d12ea74
YesunPark <lynn08082@gmail.com>
2023-08-28 00:46:04 +0900

    Test: PayServiceTest BeforeEach 메서드 생성

--- src/test/java/com/zerobase/cafebom/pay/service/PayServiceTest.java
+++ src/test/java/com/zerobase/cafebom/pay/service/PayServiceTest.java
@@ -2,8 +2,14 @@ package com.zerobase.cafebom.pay.service;
 
 import static org.mockito.BDDMockito.given;
 
+import com.zerobase.cafebom.member.domain.entity.Member;
 import com.zerobase.cafebom.member.repository.MemberRepository;
+import com.zerobase.cafebom.orders.domain.type.Payment;
+import com.zerobase.cafebom.pay.service.dto.OrdersAddDto;
+import com.zerobase.cafebom.pay.service.dto.OrdersAddDto.OrderedProductDto;
 import com.zerobase.cafebom.security.TokenProvider;
+import java.util.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,17 +35,32 @@ class PayServiceTest {
 
     String token = "Bearer token";
 
+    OrdersAddDto.Request request;
+
+    @BeforeEach
+    public void setUp() {
+        request = OrdersAddDto.Request.builder()
+            .payment(Payment.KAKAO_PAY)
+            .products(Collections.singletonList(OrderedProductDto.builder()
+                .productId(1)
+                .optionIds(Arrays.asList(1, 2))
+                .build()))
+            .build();
+        given(tokenProvider.getId(token)).willReturn(2L);
+    }
+
 
     @Test
     @DisplayName("주문 저장 성공")
     void successAddOrders() {
         // given
-//        given(tokenProvider.getId(token)).willReturn(2L);
-//        given(memberRepository.findById(request.getUserId()))
-//            .willReturn(Optional.ofNullable(userEntity));
+        given(memberRepository.findById(2L))
+            .willReturn(Optional.ofNullable(Member.builder()
+                .build()));
 //        given(accountRepository.save(any())).willReturn(accountEntity);
 
         // when
+        payService.addOrders(token, request);
 
         // then
     }
