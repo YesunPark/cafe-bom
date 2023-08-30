e321a826bab18a695abb71fbcab2b38d9aa71726
Kim Youngseon <thsu1084@naver.com>
2023-08-30 17:07:02 +0900

    Feat: 주문 내역 확인하기 기능 구현

--- src/test/java/com/zerobase/cafebom/history/service/OrdersHistoryServiceTest.java
+++ src/test/java/com/zerobase/cafebom/history/service/OrdersHistoryServiceTest.java
@@ -21,9 +21,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT) // 추가
 class OrdersHistoryServiceTest {
 
     @InjectMocks
