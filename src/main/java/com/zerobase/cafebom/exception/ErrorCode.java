addbfa7118e6495f40c0863610a437e0140a245a
KWY <sunlake123@naver.com>
2023-08-30 19:36:42 +0900

    Refactor: productController, productService -> product로 통합

--- src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
+++ src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
@@ -14,10 +14,8 @@ public enum ErrorCode {
     PHONE_ALREADY_EXISTS("이미 존재하는 휴대전화번호입니다.", HttpStatus.BAD_REQUEST),
     PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
 
-    // ProductController
+    // Product
     METHOD_ARGUMENT_TYPE_MISMATCH("메서드 매개변수 타입이 맞지 않습니다.", HttpStatus.BAD_REQUEST),
-
-    // ProductService
     PRODUCTCATEGORY_NOT_FOUND("존재하지 않는 상품 카테고리입니다.", HttpStatus.BAD_REQUEST);
 
     private final String message;
