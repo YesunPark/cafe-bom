78bd86fa345ce027026be6acdb89b057f34e4607
rkoji <wl0wn1212@gmail.com>
2023-08-30 21:50:50 +0900

    Refactor : soldOutstatus 자료형을 SoldOutstatus enum type으로 수정

--- src/main/java/com/zerobase/cafebom/product/controller/form/SoldOutStatusForm.java
+++ src/main/java/com/zerobase/cafebom/product/controller/form/SoldOutStatusForm.java
@@ -1,19 +1,18 @@
 package com.zerobase.cafebom.product.controller.form;
 
+import com.zerobase.cafebom.product.domain.type.SoldOutStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
 public class SoldOutStatusForm {
 
     @Schema(description = "품절여부", example = "SOLD_OUT")
-    @Pattern(regexp = "^[A-Z_]+$", message = "품절여부는 영어 대문자와 언더스코어(_)만 입력 가능합니다")
-    @NotBlank(message = "품절여부는 필수로 입력해야 합니다.")
-    private String soldOutStatus;
+    @NotNull(message = "품절여부는 필수로 입력해야 합니다.")
+    private SoldOutStatus soldOutStatus;
 
 }
