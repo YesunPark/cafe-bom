597f6936c77353d99d254103909e9231118fe5b5
rkoji <wl0wn1212@gmail.com>
2023-08-31 18:44:44 +0900

    Refactor : 리뷰 반영 수정 ->  조회 메서드에서 return ResponseEntity.ok().body();로 통일

--- src/main/java/com/zerobase/cafebom/option/controller/OptionController.java
+++ src/main/java/com/zerobase/cafebom/option/controller/OptionController.java
@@ -6,7 +6,6 @@ import com.zerobase.cafebom.option.service.OptionService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -35,14 +34,14 @@ public class OptionController {
     @GetMapping
     public ResponseEntity<?> optionList() {
         List<OptionForm.Response> optionList = optionService.findAllOption();
-        return  ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(optionList);
+        return  ResponseEntity.ok().body(optionList);
     }
 
     @ApiOperation(value = "옵션Id별 조회(관리자)", notes = "관리자가 옵션Id를 통해 조회합니다.")
     @GetMapping("/{id}")
     public ResponseEntity<?> optionListById(@PathVariable Integer id){
         OptionForm.Response response = optionService.findByIdOption(id);
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+        return ResponseEntity.ok().body(response);
     }
 
 }
