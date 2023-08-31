f87abf5e92be1c33ecb557e836488fff102a30e7
YesunPark <lynn08082@gmail.com>
2023-09-01 03:43:25 +0900

    Fix: 리뷰 엔티티 생성

--- src/main/java/com/zerobase/cafebom/review/domain/entity/Review.java
+++ src/main/java/com/zerobase/cafebom/review/domain/entity/Review.java
@@ -1,20 +1,11 @@
 package com.zerobase.cafebom.review.domain.entity;
 
 import com.zerobase.cafebom.common.BaseTimeEntity;
-import com.zerobase.cafebom.member.domain.entity.Member;
-import com.zerobase.cafebom.orders.domain.type.OrdersCookingStatus;
-import com.zerobase.cafebom.orders.domain.type.OrdersCookingTime;
-import com.zerobase.cafebom.orders.domain.type.OrdersReceiptStatus;
-import com.zerobase.cafebom.orders.domain.type.Payment;
-import java.time.LocalDateTime;
+import com.zerobase.cafebom.ordersproduct.domain.entity.OrdersProduct;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -33,23 +24,16 @@ public class Review extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    private Long memberId;
-//
-//    @NotNull
-//    @Enumerated(EnumType.STRING)
-//    private Payment payment;
-//
-//    @NotNull
-//    @Enumerated(EnumType.STRING)
-//    private OrdersCookingStatus cookingStatus;
-//
-//    @Enumerated(EnumType.STRING)
-//    private OrdersCookingTime cookingTime;
-//
-//    @NotNull
-//    @Enumerated(EnumType.STRING)
-//    private OrdersReceiptStatus receiptStatus;
-//
-//    private LocalDateTime receivedTime;
+    @NotNull
+    private Long memberId;
 
+    @NotNull
+    private OrdersProduct ordersProduct;
+
+    @NotNull
+    private Integer rating;
+
+    private String content;
+
+    private String picture;
 }
