0ccc256ed0d038ae026ffa8d4da7c5d79727ab5c
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-09-02 14:02:16 +0900

    Feat : 관리자 Option 조회, 삭제기능 추가 (#44)
    * Feat : 관리자 Option 조회, 삭제기능 추가
    
    * Refactor : 리뷰 반영 Controller, Serive수정
    
    * Feat : OptionForm 생성
    
    * Refactor : 리뷰 반영 수정 메서드명 수정
    
    * Refactor : 리뷰 반영 수정 ->  조회 메서드에서 return ResponseEntity.ok().body();로 통일
    
    * Refactor : main 병합 후 불필요하고 중복되는 코드 제거, Form Dto 하나로 통합

--- src/main/java/com/zerobase/cafebom/option/controller/OptionController.java
+++ src/main/java/com/zerobase/cafebom/option/controller/OptionController.java
@@ -1,10 +1,8 @@
 package com.zerobase.cafebom.option.controller;
 
-import com.zerobase.cafebom.option.controller.form.OptionAddForm;
-import com.zerobase.cafebom.option.controller.form.OptionModifyForm;
+import com.zerobase.cafebom.option.controller.form.OptionForm;
 import com.zerobase.cafebom.option.service.OptionService;
-import com.zerobase.cafebom.option.service.dto.OptionAddDto;
-import com.zerobase.cafebom.option.service.dto.OptionModifyDto;
+import com.zerobase.cafebom.option.service.dto.OptionDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "product-controller", description = "관리자 옵션 CRUD API")
 @Controller
@@ -25,8 +25,8 @@ public class OptionController {
     // jiyeon-23.08.30
     @ApiOperation(value = "옵션 등록(관리자)", notes = "관리자가 옵션을 등록합니다.")
     @PostMapping
-    public ResponseEntity<?> optionAdd(@RequestBody OptionAddForm optionAddForm) {
-        optionService.addOption(OptionAddDto.from(optionAddForm));
+    public ResponseEntity<?> optionAdd(@RequestBody OptionForm.Request optionFormRequest) {
+        optionService.addOption(OptionDto.Request.from(optionFormRequest));
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
@@ -35,8 +35,33 @@ public class OptionController {
     @PutMapping("/{id}")
     public ResponseEntity<?> optionModify(
             @PathVariable Integer id,
-            @RequestBody OptionModifyForm form) {
-        optionService.modifyOption(id, OptionModifyDto.Request.from(form));
+            @RequestBody OptionForm.Request form) {
+        optionService.modifyOption(id, OptionDto.Request.from(form));
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+
+    // jiyeon-23.08.30
+    @ApiOperation(value = "옵션 삭제(관리자)", notes = "관리자가 옵션을 삭제합니다.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> optionRemove(@PathVariable Integer id) {
+        optionService.removeOption(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // jiyeon-23.08.30
+    @ApiOperation(value = "옵션 전체 조회(관리자)", notes = "관리자가 옵션을 전체 조회합니다.")
+    @GetMapping
+    public ResponseEntity<?> optionList() {
+        List<OptionForm.Response> optionList = optionService.findAllOption();
+        return  ResponseEntity.ok().body(optionList);
+    }
+
+    // jiyeon-23.08.30
+    @ApiOperation(value = "옵션Id별 조회(관리자)", notes = "관리자가 옵션Id를 통해 조회합니다.")
+    @GetMapping("/{id}")
+    public ResponseEntity<?> optionListById(@PathVariable Integer id){
+        OptionForm.Response response = optionService.findByIdOption(id);
+        return ResponseEntity.ok().body(response);
+    }
+
 }
--- src/main/java/com/zerobase/cafebom/option/service/OptionService.java
+++ src/main/java/com/zerobase/cafebom/option/service/OptionService.java
@@ -1,14 +1,26 @@
 package com.zerobase.cafebom.option.service;
 
-import com.zerobase.cafebom.option.service.dto.OptionModifyDto;
-import com.zerobase.cafebom.option.service.dto.OptionAddDto;
+import com.zerobase.cafebom.option.controller.form.OptionForm;
+import com.zerobase.cafebom.option.service.dto.OptionDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface OptionService {
     // 옵션 등록-jiyeon-23.08.30
-    void addOption(OptionAddDto optionAddDto);
+    void addOption(OptionDto.Request optionAddDto);
 
     // 옵션 수정-jiyeon-23.08.30
-    void modifyOption(Integer id, OptionModifyDto.Request request);
+    void modifyOption(Integer id, OptionDto.Request request);
+
+
+    // 옵션 삭제-jiyeon-23.08.30
+    void removeOption(Integer id);
+
+    // 옵션 전체 조회-jiyeon-23.08.30
+    List<OptionForm.Response> findAllOption();
+
+    // 옵션Id별 조회-jiyeon-23.08.30
+    OptionForm.Response findByIdOption(Integer id);
 }
