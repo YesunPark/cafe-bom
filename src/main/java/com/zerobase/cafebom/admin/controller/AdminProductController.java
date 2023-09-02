48de2428636ff28c5e1ae8cf1da0481b04981b9d
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-09-02 16:17:32 +0900

    Feat : 관리자 상품 등록,조회,수정,삭제 기능 구현 (#32)
    * Feat : 관리자 메뉴 등록,조회,수정,삭제 기능 구현
    
    * Rename : ProductStatus -> SoldOutStatus 변경
    
    * Rename : ProductStatus -> SoldOutStatus 변경
    
    * Rename : ProductStatus -> SoldOutStatus 변경
    
    * Feat : 회원이 관리자일때만 수정할 수 있도록 기능 추가
    
    * Feat : 회원이 관리자일때만 수정할 수 있도록 기능 추가
    
    * Feat : 회원이 관리자일때만 수정할 수 있도록 기능 추가
    
    * Fix : Service에서 사용하던 @PreAuthorize("hasRole('ADMIN')") 을 Controller로 옮김
    
    * Feat : ProductController @PreAuthorize("hasRole('ADMIN')") 추가
    
    * Test : ProductControllerTest, ProductServiceTest 추가
    
    * Refactor : PR 리뷰 반영 수정
    
    * Refactor : ProductForm Integer로 자료형 변경
    
    * Test : ProductControllerTest, ServiceTest 수정
    
    * Refactor ProductAdd 메서드 @PostMapping에서 value="/add"삭제
    
    * Feat : AmazonS3관련 gradle 추가
    
    * Refactor :  관리자 상품 삭제 메서드 리뷰 반영 수정
    
    * Refactor :  관리자 상품 등록 메서드 리뷰 반영 수정
    
    * Refactor :  관리자 상품 수정 메서드 리뷰 반영 수정
    
    * Feat :  관리자 상품 전체조회, 상품Id별 조회 서비스 기능 생성
    
    * Refactor : 리뷰 반영 수정
    
    * Remove : Test코드 삭제
    
    * Refactor : 쓰지않는 코드 삭제
    
    * Refactor : 리뷰 반영 조회 메서드에서 return ResponseEntity.ok().body();로 통일
    
    * Refactor : 주석 칸 맞추기
    
    * Rename : 관리자 메뉴 수정,삭제,조회,등록 기능을 admin패키지로 옮기면서 클래스 이름을 AdminProduct~ 로 수정
    
    * Refactor : main 병합후 불필요한 코드 삭제 및 수정

---
+++ src/main/java/com/zerobase/cafebom/admin/controller/AdminProductController.java
@@ -0,0 +1,76 @@
+package com.zerobase.cafebom.admin.controller;
+
+import com.zerobase.cafebom.admin.controller.form.AdminProductForm;
+import com.zerobase.cafebom.admin.service.AdminProductService;
+import com.zerobase.cafebom.admin.service.dto.AdminProductDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+@Tag(name = "product-controller", description = "관리자 메뉴 CRUD API")
+@Controller
+@RequestMapping("/admin/product")
+@RequiredArgsConstructor
+//@PreAuthorize("hasRole('ADMIN')")
+public class AdminProductController {
+
+    private final AdminProductService adminProductService;
+
+    // jiyeon-23.08.25
+    @ApiOperation(value = "상품 전체 조회(관리자)", notes = "관리자가 상품 전체 리스트를 조회합니다")
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> productList() {
+        List<AdminProductForm.Response> productList = adminProductService.findProductList();
+        return ResponseEntity.ok().body(productList);
+    }
+
+    // jiyeon-23.08.25
+    @ApiOperation(value = "상품 Id별 조회(관리자)", notes = "관리자가 상품 Id 별로 조회합니다.")
+    @GetMapping("/{id}")
+    public ResponseEntity<?> productIdGet(@PathVariable Integer id) {
+        AdminProductForm.Response response = adminProductService.findProductById(id);
+        return ResponseEntity.ok().body(response);
+    }
+
+    // jiyeon-23.08.25
+    @ApiOperation(value = "상품 등록(관리자)", notes = "관리자가 상품을 등록합니다.")
+    @PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> productAdd(
+            HttpServletRequest request,
+            @RequestParam(value = "image") MultipartFile image,
+            AdminProductForm adminProductForm) throws IOException {
+        adminProductService.addProduct(image, AdminProductDto.from(adminProductForm));
+        return ResponseEntity.status(NO_CONTENT).build();
+    }
+
+    // jiyeon-23.08.25
+    @ApiOperation(value = "상품 수정(관리자)", notes = "관리자가 상품Id 별로 수정합니다.")
+    @PutMapping(value = "/{id}", consumes = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> productModify(
+            @RequestParam(value = "image") MultipartFile image,
+            @PathVariable Integer id,
+            AdminProductForm adminProductForm) throws IOException {
+        adminProductService.modifyProduct(image, id, AdminProductDto.from(adminProductForm));
+        return ResponseEntity.status(NO_CONTENT).build();
+    }
+
+    // jiyeon-23.08.25
+    @ApiOperation(value = "상품 삭제(관리자)", notes = "관리자가 상품Id 별로 삭제합니다.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> productRemove(@PathVariable Integer id) {
+        adminProductService.removeProduct(id);
+        return ResponseEntity.status(NO_CONTENT).build();
+    }
+}
--- src/main/java/com/zerobase/cafebom/product/domain/entity/Product.java
+++ src/main/java/com/zerobase/cafebom/product/domain/entity/Product.java
@@ -1,24 +1,18 @@
 package com.zerobase.cafebom.product.domain.entity;
 
+import com.zerobase.cafebom.admin.service.dto.AdminProductDto;
 import com.zerobase.cafebom.common.BaseTimeEntity;
 import com.zerobase.cafebom.product.domain.type.SoldOutStatus;
 import com.zerobase.cafebom.productcategory.domain.entity.ProductCategory;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Builder
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Builder(toBuilder = true)
 @Getter
 @RequiredArgsConstructor
 @AllArgsConstructor
@@ -49,4 +43,26 @@ public class Product extends BaseTimeEntity {
     @NotNull
     private String picture;
 
+
+    public void modifyProductForm(AdminProductDto productDto, ProductCategory productCategory) {
+        if (productDto.getName() != null) {
+            this.name = productDto.getName();
+        }
+        if (productDto.getProductCategoryId() != null) {
+            this.productCategory = productCategory;
+        }
+        if (productDto.getDescription() != null) {
+            this.description = productDto.getDescription();
+        }
+        if (productDto.getPrice() != null) {
+            this.price = productDto.getPrice();
+        }
+        if (productDto.getSoldOutStatus() != null) {
+            this.soldOutStatus = productDto.getSoldOutStatus();
+        }
+    }
+
+    public void modifyNewImageUrl(String newImageUrl) {
+        this.picture = newImageUrl;
+    }
 }
