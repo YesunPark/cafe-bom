362c18fe09bbc3ff0a5d4821c08f38ab97608e3f
rkoji <wl0wn1212@gmail.com>
2023-09-02 13:19:02 +0900

    Refactor : main 병합 후, 불필요하고 중복되는 코드 제거

--- src/main/java/com/zerobase/cafebom/option/controller/OptionController.java
+++ src/main/java/com/zerobase/cafebom/option/controller/OptionController.java
@@ -1,22 +1,17 @@
 package com.zerobase.cafebom.option.controller;
 
-import com.zerobase.cafebom.option.controller.form.OptionModifyForm;
-import com.zerobase.cafebom.option.repository.OptionRepository;
-import com.zerobase.cafebom.option.service.OptionService;
-import com.zerobase.cafebom.option.service.dto.OptionModifyDto;
-import io.swagger.annotations.ApiOperation;
 import com.zerobase.cafebom.option.controller.form.OptionAddForm;
+import com.zerobase.cafebom.option.controller.form.OptionModifyForm;
 import com.zerobase.cafebom.option.service.OptionService;
 import com.zerobase.cafebom.option.service.dto.OptionAddDto;
+import com.zerobase.cafebom.option.service.dto.OptionModifyDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "product-controller", description = "관리자 옵션 CRUD API")
 @Controller
@@ -35,6 +30,7 @@ public class OptionController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    // jiyeon-23.08.30
     @ApiOperation(value = "옵션 수정(관리자)", notes = "관리자가 옵션을 수정합니다.")
     @PutMapping("/{id}")
     public ResponseEntity<?> optionModify(
--- src/main/java/com/zerobase/cafebom/option/service/OptionServiceImpl.java
+++ src/main/java/com/zerobase/cafebom/option/service/OptionServiceImpl.java
@@ -1,22 +1,21 @@
 package com.zerobase.cafebom.option.service;
 
 import com.zerobase.cafebom.exception.CustomException;
-import com.zerobase.cafebom.exception.ErrorCode;
 import com.zerobase.cafebom.option.domain.entity.Option;
 import com.zerobase.cafebom.option.repository.OptionRepository;
+import com.zerobase.cafebom.option.service.dto.OptionAddDto;
 import com.zerobase.cafebom.option.service.dto.OptionModifyDto;
 import com.zerobase.cafebom.optioncategory.domain.entity.OptionCategory;
 import com.zerobase.cafebom.optioncategory.repository.OptionCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
+import static com.zerobase.cafebom.exception.ErrorCode.NOT_FOUND_OPTION;
+import static com.zerobase.cafebom.exception.ErrorCode.NOT_FOUND_OPTION_CATEGORY;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
-public class OptionServiceImpl implements OptionService{
+public class OptionServiceImpl implements OptionService {
 
     private final OptionRepository optionRepository;
     private final OptionCategoryRepository optionCategoryRepository;
@@ -26,25 +25,24 @@ public class OptionServiceImpl implements OptionService{
     public void addOption(OptionAddDto optionAddDto) {
         Integer optionCategoryId = optionAddDto.getOptionCategoryId();
         OptionCategory optionCategory = optionCategoryRepository.findById(optionCategoryId)
-                    .orElseThrow(() -> new CustomException(NOT_FOUND_OPTION_CATEGORY));
-            Option option = Option.builder()
-                    .optionCategory(optionCategory)
-                    .name(optionAddDto.getName())
-                    .price(optionAddDto.getPrice())
-                    .build();
-            optionRepository.save(option);
-
+                .orElseThrow(() -> new CustomException(NOT_FOUND_OPTION_CATEGORY));
+        Option option = Option.builder()
+                .optionCategory(optionCategory)
+                .name(optionAddDto.getName())
+                .price(optionAddDto.getPrice())
+                .build();
+        optionRepository.save(option);
     }
-}
+
     // 옵션 수정-jiyeon-23.08.30
     @Override
     public void modifyOption(Integer id, OptionModifyDto.Request request) {
         Integer optionCategoryId = request.getOptionCategoryId();
         OptionCategory optionCategory = optionCategoryRepository.findById(optionCategoryId)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_OPTION_CATEGORY));
+                .orElseThrow(() -> new CustomException(NOT_FOUND_OPTION_CATEGORY));
 
         Option optionId = optionRepository.findById(id)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_OPTION));
+                .orElseThrow(() -> new CustomException(NOT_FOUND_OPTION));
 
         Option modifyOption = Option.builder()
                 .optionCategory(optionCategory)
