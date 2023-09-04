20048d01bf841bae19f8465e58c3b045b0b4a571
YesunPark <lynn08082@gmail.com>
2023-09-04 20:37:42 +0900

    Comment: 설명용 주석 삭제

--- src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
+++ src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
@@ -10,8 +10,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    // HttpStatus.BAD_REQUEST 에서 static import로 수정. =>  타팀에서 멘토님께 코드리뷰 받은 부분
-
     // Auth
     MEMBER_NOT_EXISTS("존재하지 않는 회원입니다.", BAD_REQUEST),
     EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다.", BAD_REQUEST),
@@ -22,29 +20,25 @@ public enum ErrorCode {
     // Orders
     ORDERS_NOT_EXISTS("존재하지 않는 주문입니다.", NOT_FOUND),
     ORDERS_NOT_COOKING_STATUS("조리 중인 주문이 아닙니다.", BAD_REQUEST),
-    ORDERS_NOT_CORRECT("올바른 주문 변경이 아닙니다.", BAD_REQUEST), // 무슨 뜻?
+    ORDERS_NOT_CORRECT("주문 상태는 이전 상태로 변경이 불가능합니다.", BAD_REQUEST),
 
     // Product
     PRODUCT_NOT_EXISTS("존재하지 않는 상품입니다.", BAD_REQUEST),
-    METHOD_ARGUMENT_TYPE_MISMATCH("메서드 매개변수 타입이 맞지 않습니다.", BAD_REQUEST), // 에러코드에 맞지 않는 에러 형식인 듯
+
+    // 컨트롤러 @PathVariable TypeMismatch
+    METHOD_ARGUMENT_TYPE_MISMATCH("메서드 매개변수의 타입이 맞지 않습니다.", BAD_REQUEST),
 
     // ProductService -> ?
-    INVALID_INPUT("잘못된 입력입니다", BAD_REQUEST), // 담당자분 누구신가여? 뭐가 잘못된 입력인지 알려줘야 할 듯 합니다!
-    // 보니까 기간으로 조회했는데 시작 날짜 또는 끝 날짜를 null 로 보낸 경우인 것 같아요
-    // 그러면 시작 날짜와 끝 날짜를 입력해야 합니다. 이런 식으로 에러메세지를 주는 게 좋지 않을까 싶네요!
-    // Ex) START_DATE_AND_END_DATE_ARE_ESSENTIAL("시작 날짜와 끝 날짜를 입력해야 합니다.", BAD_REQUEST),
+    START_DATE_AND_END_DATE_ARE_ESSENTIAL("시작 날짜와 끝 날짜를 입력해야 합니다.", BAD_REQUEST),
 
     // Cart
     CART_IS_EMPTY("장바구니에 담긴 상품이 없습니다.", BAD_REQUEST),
 
     // ProductCategory
     PRODUCTCATEGORY_NOT_EXISTS("존재하지 않는 상품 카테고리입니다.", BAD_REQUEST),
-//    NOT_FOUND_PRODUCT_CATEGORY("상품 카테고리가 존재하지 않습니다.", HttpStatus.BAD_REQUEST), => 위에 걸로 통합 완료
 
     // Option
     OPTION_NOT_EXISTS("존재하지 않는 옵션입니다.", BAD_REQUEST),
-    //    NOT_FOUND_OPTION("옵션을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST), => 위에 걸로 통합 완료
-//    NOT_FOUND_OPTION_ID("옵션을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST), => 위에 걸로 통합 완료
     NOT_FOUND_OPTION_CATEGORY("옵션 카테고리를 찾을 수 없습니다.", BAD_REQUEST);
 
     private final String message;
