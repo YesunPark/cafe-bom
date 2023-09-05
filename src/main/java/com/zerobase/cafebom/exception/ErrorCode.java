3210e77e1bdc58dd3c9d69f0469800571fc83699
bingsoo95 <kimminsu403@naver.com>
2023-09-06 01:53:10 +0900

    Refactor: ErrorCode 파일 HttpStatus static Import 수정

--- src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
+++ src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
@@ -23,12 +23,12 @@ public enum ErrorCode {
 
     // Orders
     ORDERS_NOT_EXISTS("존재하지 않는 주문입니다.", NOT_FOUND),
-    ORDERS_ALREADY_COOKING_STATUS("이미 조리 중인 주문입니다.", HttpStatus.BAD_REQUEST),
+    ORDERS_ALREADY_COOKING_STATUS("이미 조리 중인 주문입니다.", BAD_REQUEST),
     ORDERS_NOT_COOKING_STATUS("조리 중인 주문이 아닙니다.", BAD_REQUEST),
     ORDERS_NOT_CORRECT("주문 상태는 이전 상태로 변경이 불가능합니다.", BAD_REQUEST),
-    ORDERS_ALREADY_CANCELED("이미 취소된 주문입니다.", HttpStatus.BAD_REQUEST),
-    ORDERS_NOT_RECEIVED_STATUS("수락되지 않은 주문입니다.", HttpStatus.BAD_REQUEST),
-    ORDERS_COOKING_TIME_ALREADY_SET("이미 조리 예정 시간이 설정되어 있습니다.", HttpStatus.BAD_REQUEST),
+    ORDERS_ALREADY_CANCELED("이미 취소된 주문입니다.", BAD_REQUEST),
+    ORDERS_NOT_RECEIVED_STATUS("수락되지 않은 주문입니다.", BAD_REQUEST),
+    ORDERS_COOKING_TIME_ALREADY_SET("이미 조리 예정 시간이 설정되어 있습니다.", BAD_REQUEST),
     START_DATE_AND_END_DATE_ARE_ESSENTIAL("시작 날짜와 끝 날짜를 입력해야 합니다.", BAD_REQUEST),
 
     // Product
