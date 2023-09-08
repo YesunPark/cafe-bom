fd5665f7746b0b7fa472de414c1ac914488a0a08
rkoji <wl0wn1212@gmail.com>
2023-09-08 15:46:10 +0900

    Refactor : main 병합 후 불필요한 코드 수정,삭제

--- src/main/java/com/zerobase/cafebom/admin/controller/AdminProductController.java
+++ src/main/java/com/zerobase/cafebom/admin/controller/AdminProductController.java
@@ -1,23 +1,24 @@
 package com.zerobase.cafebom.admin.controller;
 
+import com.zerobase.cafebom.admin.dto.AdminProductDto;
 import com.zerobase.cafebom.admin.dto.AdminProductForm;
 import com.zerobase.cafebom.admin.service.AdminProductService;
+import com.zerobase.cafebom.type.SoldOutStatus;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @Tag(name = "admin-product-controller", description = "관리자 상품 CRUD API")
 @Controller
@@ -68,8 +69,18 @@ public class AdminProductController {
     // jiyeon-23.09.05
     @ApiOperation(value = "상품 삭제", notes = "관리자가 상품을 삭제합니다.")
     @DeleteMapping("/{productId}")
-    public ResponseEntity<?> productRemove(@PathVariable Integer productId) {
+    public ResponseEntity<?> productRemove(@PathVariable Integer productId) throws IOException {
         adminProductService.removeProduct(productId);
+        return ResponseEntity.status(NO_CONTENT).build();
+    }
+
+    // jiyeon-23.08.29
+    @ApiOperation(value = "상품 품절여부 수정", notes = "관리자가 상품의 품절여부를 수정합니다.")
+    @PatchMapping
+    public ResponseEntity<Void> productSoldOutModify(
+            @RequestParam(name = "productId") Integer productId,
+            @RequestParam(name = "soldOutStatus") SoldOutStatus soldOutStatus) {
+        adminProductService.modifyProductSoldOut(productId, soldOutStatus);
         return ResponseEntity.status(NO_CONTENT).build();
     }
 }
