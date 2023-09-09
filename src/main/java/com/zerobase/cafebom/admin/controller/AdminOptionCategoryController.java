d694bde76296559c5c0b35092383ed6cb3fa0e27
rkoji <wl0wn1212@gmail.com>
2023-09-09 17:04:09 +0900

    Refactor : Controller에 return값이 빠져 추가

--- src/main/java/com/zerobase/cafebom/admin/controller/AdminOptionCategoryController.java
+++ src/main/java/com/zerobase/cafebom/admin/controller/AdminOptionCategoryController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+
 @Tag(name = "admin-option-category-controller", description = "관리자 옵션 카테고리 CRUD API")
 @Controller
 @RequiredArgsConstructor
@@ -29,6 +31,6 @@ public class AdminOptionCategoryController {
             @PathVariable Integer optionCategoryId,
             @RequestBody AdminOptionCategoryForm.Request optionCategoryForm) {
         adminOptionCategoryService.modifyOptionCategory(optionCategoryId, optionCategoryForm);
-        return null;
+        return ResponseEntity.status(NO_CONTENT).build();
     }
 }
