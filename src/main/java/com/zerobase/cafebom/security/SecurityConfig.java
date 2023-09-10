b933a8450d61f9e4259fadaa196066cd6b15ee20
YesunPark <lynn08082@gmail.com>
2023-09-10 22:53:28 +0900

    Fix: 시큐리티 설정 파일명 변경, hasRole에서 hasAuthority로 변경

--- src/main/java/com/zerobase/cafebom/security/SecurityConfig.java
+++ src/main/java/com/zerobase/cafebom/security/SecurityConfig.java
@@ -1,42 +1,43 @@
 package com.zerobase.cafebom.security;
 
+import static com.zerobase.cafebom.security.Role.ROLE_ADMIN;
+import static com.zerobase.cafebom.security.Role.ROLE_USER;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
 @RequiredArgsConstructor
-//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @Slf4j
-public class SecurityConfiguration {
+public class SecurityConfig {
 
-    @Bean
-    PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final TokenProvider tokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.csrf(AbstractHttpConfigurer::disable)
             .cors(Customizer.withDefaults()) // CORS 설정 허용
             .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable).disable())
             .authorizeHttpRequests(auth -> auth
                 .antMatchers("/**/signup/**", "/**/signin", "/product/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/auth/**").hasRole("USER")
-                .antMatchers("/admin/**").hasRole("ADMIN"));
-
+                .antMatchers("/auth/**").hasAuthority(ROLE_USER.name())
+                .antMatchers("/admin/**").hasAuthority(ROLE_ADMIN.name())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(tokenProvider),
+                    UsernamePasswordAuthenticationFilter.class)
+            );
         return http.build();
     }
 }
