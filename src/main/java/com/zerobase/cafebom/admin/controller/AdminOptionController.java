319ae07b6ba50fe8de70816dddcc47ad1d30bf76
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-09-18 17:32:17 +0900

    Refactor : 관리자 상품, 옵션, 상품카테고리 리팩토링 (#71)
    * Refactor : 상품 카테고리가 이미 존재할 경우 예외처리 추가
    
    * Refactor : AdminProduct 컨벤션에 따라 리팩토링
    
    * Refactor : AdminProductCategory, AdminOption 리팩토링 수정
    
    * Refactor : 옵션카테고리 에러코드 수정

--- src/main/java/com/zerobase/cafebom/admin/controller/AdminOptionController.java
+++ src/main/java/com/zerobase/cafebom/admin/controller/AdminOptionController.java
@@ -1,26 +1,19 @@
 package com.zerobase.cafebom.admin.controller;
 
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-
 import com.zerobase.cafebom.option.dto.OptionDto;
 import com.zerobase.cafebom.option.dto.OptionForm;
 import com.zerobase.cafebom.option.service.OptionService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @Tag(name = "admin-option-controller", description = "관리자 옵션 CRUD API")
 @Controller
@@ -30,44 +23,44 @@ public class AdminOptionController {
 
     private final OptionService optionService;
 
-    // jiyeon-23.09.05
+    // jiyeon-23.09.13
     @ApiOperation(value = "옵션 등록", notes = "관리자가 옵션을 등록합니다.")
     @PostMapping
-    public ResponseEntity<?> optionAdd(@RequestBody OptionForm.Request optionFormRequest) {
+    public ResponseEntity<Void> optionAdd(@RequestBody OptionForm.Request optionFormRequest) {
         optionService.addOption(OptionDto.Request.from(optionFormRequest));
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(CREATED).build();
     }
 
-    // jiyeon-23.09.05
+    // jiyeon-23.09.13
     @ApiOperation(value = "옵션 수정", notes = "관리자가 옵션의 정보를 수정합니다.")
     @PutMapping("/{optionId}")
-    public ResponseEntity<?> optionModify(
+    public ResponseEntity<Void> optionModify(
         @PathVariable Integer optionId,
         @RequestBody OptionForm.Request form) {
         optionService.modifyOption(optionId, OptionDto.Request.from(form));
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(NO_CONTENT).build();
     }
 
-    // jiyeon-23.09.05
+    // jiyeon-23.09.13
     @ApiOperation(value = "옵션 삭제", notes = "관리자가 옵션을 삭제합니다.")
     @DeleteMapping("/{optionId}")
-    public ResponseEntity<?> optionRemove(@PathVariable Integer optionId) {
+    public ResponseEntity<Void> optionRemove(@PathVariable Integer optionId) {
         optionService.removeOption(optionId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(NO_CONTENT).build();
     }
 
-    // jiyeon-23.09.05
+    // jiyeon-23.09.13
     @ApiOperation(value = "옵션 전체 조회", notes = "관리자가 전체 옵션 목록을 조회합니다.")
     @GetMapping("/list")
-    public ResponseEntity<?> optionList() {
+    public ResponseEntity<List<OptionForm.Response>> optionList() {
         List<OptionForm.Response> optionList = optionService.findAllOption();
         return ResponseEntity.ok().body(optionList);
     }
 
-    // jiyeon-23.09.05
+    // jiyeon-23.09.13
     @ApiOperation(value = "옵션 단건 조회", notes = "관리자가 하나의 옵션에 대한 정보를 조회합니다.")
     @GetMapping("/{optionId}")
-    public ResponseEntity<?> optionListById(@PathVariable Integer optionId) {
+    public ResponseEntity<OptionForm.Response> optionListById(@PathVariable Integer optionId) {
         OptionForm.Response response = optionService.findByIdOption(optionId);
         return ResponseEntity.ok().body(response);
     }
