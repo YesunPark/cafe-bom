81123f0c8cf191d1983d6808ff0ea98b8dfcb4ae
YesunPark <lynn08082@gmail.com>
2023-09-19 22:00:38 +0900

    Fix: Auth 관련 중복 ErrorCode 409로 수정

--- src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
+++ src/main/java/com/zerobase/cafebom/exception/ErrorCode.java
@@ -14,9 +14,9 @@ public enum ErrorCode {
 
     // Auth
     MEMBER_NOT_EXISTS("존재하지 않는 회원입니다.", BAD_REQUEST),
-    EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다.", BAD_REQUEST),
-    NICKNAME_ALREADY_EXISTS("이미 존재하는 닉네임입니다.", BAD_REQUEST),
-    PHONE_ALREADY_EXISTS("이미 존재하는 휴대전화번호입니다.", BAD_REQUEST),
+    EMAIL_ALREADY_EXISTS("이미 존재하는 이메일입니다.", CONFLICT),
+    NICKNAME_ALREADY_EXISTS("이미 존재하는 닉네임입니다.", CONFLICT),
+    PHONE_ALREADY_EXISTS("이미 존재하는 휴대전화번호입니다.", CONFLICT),
     PASSWORD_NOT_MATCH("비밀번호가 일치하지 않습니다.", BAD_REQUEST),
     ADMIN_CODE_NOT_MATCH("관리자 인증코드가 일치하지 않습니다.", BAD_REQUEST),
 
@@ -36,7 +36,7 @@ public enum ErrorCode {
 
     // Cart
     CART_IS_EMPTY("장바구니에 담긴 상품이 없습니다.", BAD_REQUEST),
-    CART_DOES_NOT_EXIST("존재하지 않는 장바구니입니다.",BAD_REQUEST),
+    CART_DOES_NOT_EXIST("존재하지 않는 장바구니입니다.", BAD_REQUEST),
 
     // ProductCategory
     PRODUCTCATEGORY_NOT_EXISTS("존재하지 않는 상품 카테고리입니다.", BAD_REQUEST),
