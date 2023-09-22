94c29e0e5a67a8df65e702f8e109f45af109b5cc
jiyeon <101394751+rkoji@users.noreply.github.com>
2023-09-23 04:08:28 +0900

    Refactor : 관리자 권한 기능 부여 어노테이션 추가 및 에러코드 수정 (#84)
    * Refactor : 컨벤션 리팩토링에 따른 수정

--- src/main/java/com/zerobase/cafebom/admin/product/controller/AdminOptionController.java
+++ src/main/java/com/zerobase/cafebom/admin/product/controller/AdminOptionController.java
@@ -1,21 +1,17 @@
 package com.zerobase.cafebom.admin.product.controller;
 
-import com.zerobase.cafebom.front.product.dto.OptionDto;
-import com.zerobase.cafebom.front.product.dto.OptionForm;
-import com.zerobase.cafebom.front.product.service.OptionService;
+import com.zerobase.cafebom.admin.product.dto.AdminOptionDto;
+import com.zerobase.cafebom.admin.product.dto.AdminOptionForm;
+import com.zerobase.cafebom.admin.product.service.AdminOptionService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
@@ -23,51 +19,53 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @Tag(name = "admin-option-controller", description = "관리자 옵션 CRUD API")
 @Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/admin/option")
 @RequiredArgsConstructor
 public class AdminOptionController {
 
-    private final OptionService optionService;
+    private final AdminOptionService adminOptionService;
 
-    // jiyeon-23.09.13
+    // jiyeon-23.09.22
     @ApiOperation(value = "옵션 등록", notes = "관리자가 옵션을 등록합니다.")
     @PostMapping
-    public ResponseEntity<Void> optionAdd(@RequestBody OptionForm.Request optionFormRequest) {
-        optionService.addOption(OptionDto.Request.from(optionFormRequest));
+    public ResponseEntity<Void> optionAdd(
+            @Valid @RequestBody AdminOptionForm.Request optionFormRequest) {
+        adminOptionService.addOption(AdminOptionDto.Request.from(optionFormRequest));
         return ResponseEntity.status(CREATED).build();
     }
 
-    // jiyeon-23.09.13
+    // jiyeon-23.09.22
     @ApiOperation(value = "옵션 수정", notes = "관리자가 옵션의 정보를 수정합니다.")
     @PutMapping("/{optionId}")
     public ResponseEntity<Void> optionModify(
         @PathVariable Integer optionId,
-        @RequestBody OptionForm.Request form) {
-        optionService.modifyOption(optionId, OptionDto.Request.from(form));
+        @Valid @RequestBody AdminOptionForm.Request form) {
+        adminOptionService.modifyOption(optionId, AdminOptionDto.Request.from(form));
         return ResponseEntity.status(NO_CONTENT).build();
     }
 
-    // jiyeon-23.09.13
+    // jiyeon-23.09.22
     @ApiOperation(value = "옵션 삭제", notes = "관리자가 옵션을 삭제합니다.")
     @DeleteMapping("/{optionId}")
     public ResponseEntity<Void> optionRemove(@PathVariable Integer optionId) {
-        optionService.removeOption(optionId);
+        adminOptionService.removeOption(optionId);
         return ResponseEntity.status(NO_CONTENT).build();
     }
 
-    // jiyeon-23.09.13
+    // jiyeon-23.09.22
     @ApiOperation(value = "옵션 전체 조회", notes = "관리자가 전체 옵션 목록을 조회합니다.")
     @GetMapping("/list")
-    public ResponseEntity<List<OptionForm.Response>> optionList() {
-        List<OptionForm.Response> optionList = optionService.findAllOption();
+    public ResponseEntity<List<AdminOptionForm.Response>> optionList() {
+        List<AdminOptionForm.Response> optionList = adminOptionService.findAllOption();
         return ResponseEntity.ok().body(optionList);
     }
 
-    // jiyeon-23.09.13
+    // jiyeon-23.09.22
     @ApiOperation(value = "옵션 단건 조회", notes = "관리자가 하나의 옵션에 대한 정보를 조회합니다.")
     @GetMapping("/{optionId}")
-    public ResponseEntity<OptionForm.Response> optionListById(@PathVariable Integer optionId) {
-        OptionForm.Response response = optionService.findByIdOption(optionId);
+    public ResponseEntity<AdminOptionForm.Response> optionListById(@PathVariable Integer optionId) {
+        AdminOptionForm.Response response = adminOptionService.findByIdOption(optionId);
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/zerobase/cafebom/admin/product/controller/AdminProductController.java
+++ src/main/java/com/zerobase/cafebom/admin/product/controller/AdminProductController.java
@@ -1,33 +1,30 @@
 package com.zerobase.cafebom.admin.product.controller;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
-
 import com.zerobase.cafebom.admin.product.dto.AdminProductDto;
 import com.zerobase.cafebom.admin.product.dto.AdminProductForm;
 import com.zerobase.cafebom.admin.product.service.AdminProductService;
 import com.zerobase.cafebom.common.type.SoldOutStatus;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @Tag(name = "admin-product-controller", description = "관리자 상품 CRUD API")
 @Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/admin/product")
 @RequiredArgsConstructor
 public class AdminProductController {
@@ -50,12 +47,12 @@ public class AdminProductController {
         return ResponseEntity.ok().body(response);
     }
 
-    // jiyeon-23.09.13
+    // jiyeon-23.09.22
     @ApiOperation(value = "상품 등록", notes = "관리자가 상품을 등록합니다.")
     @PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> productAdd(
         @RequestParam(value = "image") MultipartFile image,
-        AdminProductForm adminProductForm) throws IOException {
+        @Valid AdminProductForm adminProductForm) throws IOException {
         adminProductService.addProduct(image, AdminProductDto.from(adminProductForm));
         return ResponseEntity.status(CREATED).build();
     }
