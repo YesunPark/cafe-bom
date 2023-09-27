ad61f01a788a59b866b1e0c365c3f3ef8132ffa7
YesunPark <lynn08082@gmail.com>
2023-09-27 18:16:59 +0900

    Docs: build.gradle 버전 수정, swagger description 추가

--- src/main/java/com/zerobase/cafebom/common/config/SwaggerConfig.java
+++ src/main/java/com/zerobase/cafebom/common/config/SwaggerConfig.java
@@ -24,42 +24,48 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.zerobase.cafebom"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .ignoredParameterTypes(Errors.class);
+            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()))
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.zerobase.cafebom"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo())
+            .ignoredParameterTypes(Errors.class);
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Cafe-Bom Api Test")
-                .description("Cafe-Bom REST API")
-                .version("1.0.1")
-                .contact(new Contact("Cafe-Bom GitHub", "https://github.com/YesunPark/cafe-bom", ""))
-                .build();
+            .title("Cafe-Bom Api Test")
+            .description("Cafe-Bom REST API입니다.<br>"
+                + "<br>"
+                + "권한이 필요한 기능은 회원가입, 로그인 API로 로그인 후 받은 토큰으로 테스트가 가능합니다. <br>"
+                + "토큰 입력 방법 : **Bearer {받은 토큰}**  <br>"
+                + "<br>"
+                + "관리자 기능 : 토큰을 우측 초록색 Authorize 버튼에만 넣으면 됩니다.<br> "
+                + "사용자 기능 : 토큰을 Authorize 버튼과 API 헤더 Authorize 두 군데에 넣어야합니다.<br>  ")
+            .version("1.0.2")
+            .contact(new Contact("Cafe-Bom GitHub", "https://github.com/YesunPark/cafe-bom", ""))
+            .build();
     }
 
     // swagger-ui 페이지 연결 핸들러 설정
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
-                .addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+            .addResourceHandler("/swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
 
         registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+            .addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     // swagger Authorize 버튼 생성을 위한 기능
     private SecurityContext securityContext() {
         return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
+            .securityReferences(defaultAuth())
+            .build();
     }
 
     // swagger Authorize 버튼 생성을 위한 기능
